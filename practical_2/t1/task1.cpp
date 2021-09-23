cfc11c162a4e3f949d5f6d2d94ea81eec3e28613
nishant-out <79033833+nishant-out@users.noreply.github.com>
2021-09-23 10:53:14 +0530

    Add files via upload

---
+++ practical_2/t1/task1.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h>
+using namespace std ;
+
+int main(){
+
+	cout << "Running" << endl ;
+
+	return 0;
+}
---
+++ practical_2/t3/task3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+	string str ;
+	char c ;
+
+	cin >> c ;
+	cout << "c: " << c << endl ;
+
+	getline( cin, str ) ;
+	cout << "str: " << str << endl ;
+
+	return 0;
+}
