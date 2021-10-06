312ca72b0b9834f75f174af64bc5e3252812c40f
nishant-out <79033833+nishant-out@users.noreply.github.com>
2021-10-06 10:24:13 +0530

    Add files via upload

---
+++ practical_6/task1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+    int a = 10, b = 5 ;
+
+  //  cout << a << " + " << b << " = " << a + b << endl ;       15
+  
+  //  cout << "str" + "ing" << endl ;
+    
+    cout << a + "string" << endl ;
+
+    return 0 ;
+}
