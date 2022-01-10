febc5e6fad5e25000d659a2734abfa1a785c8e73
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-01-10 17:50:19 +0530

    Create task_15.2b.cpp

---
+++ practical_15/task_15.2b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std ;
+
+int main()
+{
+	fstream myfile;
+	myfile.open("text.txt");
+	
+	myfile<< "writing in a file using fstream";
+	myfile.close();
+	
+	return 0;
+}
+
