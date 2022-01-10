50f3a84298e9c19957a2c6cbe51f04e5daa8486f
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-01-10 16:54:08 +0530

    Create task_13.2b.cpp

---
+++ practical_13/task_13.2b.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Base{
+    public:
+    
+    virtual int add( int x, int y ){     // virtual keyword: complier will ignore fn during compile time
+        return x + y;
+    }
+
+    virtual void pure() = 0;
+};
+class Child: public Base{
+    public:
+    
+    int add( int x, int y ){
+        return x + y +1;
+    }
+    
+    void pure(  ){
+        cout << "Def. of pure virtual fn" << endl;
+    }
+};
+
+int main(){
+    
+    Base *p ;
+    Child obj;
+    
+    p = &obj;
+    
+    p-> pure();
+    cout << "sum: " << p-> add( 5, 5 ) << endl; 
+    //  Due to virtual keyword o/p is 11 
+    //  else 10
+}
+
+
+
+
+
+
+
